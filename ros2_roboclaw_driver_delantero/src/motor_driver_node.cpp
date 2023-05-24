e057b9e17b3c9fee2558267bfe61e7cf77de0be5
fcarbonero <133966447+fcarbonero@users.noreply.github.com>
2023-05-24 10:16:52 +0000

    Add files via upload
    Código para probar en pc. Roboclaw ROS2

---
+++ ros2_roboclaw_driver_delantero/src/motor_driver_node.cpp
@@ -0,0 +1,90 @@
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+
+#include "motor_driver.h"
+#include "ros2_roboclaw_driver_delantero/msg/robo_claw_status.hpp"
+#include "roboclaw.h"
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("ros2_roboclaw_driver_node");
+  MotorDriver &motorDriver = MotorDriver::singleton();
+  motorDriver.onInit(node);
+
+  auto qos = rclcpp::QoS(
+      rclcpp::QoSInitialization(
+          RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+          10));
+  qos.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+  qos.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+  qos.avoid_ros_namespace_conventions(false);
+
+  std::string statusTopicName;
+  node->declare_parameter<std::string>("roboclaw_status_topic", "roboclaw_status");
+  node->get_parameter("roboclaw_status_topic", statusTopicName);
+  RCUTILS_LOG_INFO("[motor_driver_node] roboclaw_status_topic: %s", statusTopicName.c_str());
+
+  rclcpp::Publisher<ros2_roboclaw_driver_delantero::msg::RoboClawStatus>::SharedPtr statusPublisher = node->create_publisher<ros2_roboclaw_driver_delantero::msg::RoboClawStatus>(statusTopicName, qos);
+
+  ros2_roboclaw_driver_delantero::msg::RoboClawStatus roboClawStatus;
+  rclcpp::WallRate loop_rate(20);
+  while (rclcpp::ok())
+  {
+    try
+    {
+      roboClawStatus.logic_battery_voltage = RoboClaw::singleton()->getLogicBatteryLevel();
+      roboClawStatus.main_battery_voltage = RoboClaw::singleton()->getMainBatteryLevel();
+      RoboClaw::TMotorCurrents motorCurrents = RoboClaw::singleton()->getMotorCurrents();
+      roboClawStatus.m1_motor_current = motorCurrents.m1Current;
+      roboClawStatus.m2_motor_current = motorCurrents.m2Current;
+
+      RoboClaw::TPIDQ pidq = RoboClaw::singleton()->getPIDQ(RoboClaw::kGETM1PID);
+      roboClawStatus.m1_p = pidq.p / 65536.0;
+      roboClawStatus.m1_i = pidq.i / 65536.0;
+      roboClawStatus.m1_d = pidq.d / 65536.0;
+      roboClawStatus.m1_qpps = pidq.qpps;
+
+      pidq = RoboClaw::singleton()->getPIDQ(RoboClaw::kGETM2PID);
+      roboClawStatus.m2_p = pidq.p / 65536.0;
+      roboClawStatus.m2_i = pidq.i / 65536.0;
+      roboClawStatus.m2_d = pidq.d / 65536.0;
+      roboClawStatus.m2_qpps = pidq.qpps;
+
+      roboClawStatus.temperature = RoboClaw::singleton()->getTemperature();
+
+      {
+        RoboClaw::EncodeResult encoder = RoboClaw::singleton()->getEncoderCommandResult(RoboClaw::kGETM1ENC);
+        roboClawStatus.m1_encoder_value = encoder.value;
+        roboClawStatus.m1_encoder_status = encoder.status;
+      }
+
+      {
+        RoboClaw::EncodeResult encoder = RoboClaw::singleton()->getEncoderCommandResult(RoboClaw::kGETM2ENC);
+        roboClawStatus.m2_encoder_value = encoder.value;
+        roboClawStatus.m2_encoder_status = encoder.status;
+      }
+
+      roboClawStatus.m1_current_speed = RoboClaw::singleton()->getVelocity(RoboClaw::kGETM1SPEED);
+      roboClawStatus.m2_current_speed = RoboClaw::singleton()->getVelocity(RoboClaw::kGETM2SPEED);
+
+      roboClawStatus.error_string = RoboClaw::singleton()->getErrorString();
+
+      statusPublisher->publish(roboClawStatus);
+    }
+    catch (RoboClaw::TRoboClawException *e)
+    {
+      RCUTILS_LOG_ERROR("[motor_driver_node] Exception: %s", e->what());
+    }
+    catch (...)
+    {
+      RCUTILS_LOG_ERROR("[motor_driver_node] Uncaught exception !!!");
+    }
+
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ ros2_roboclaw_driver_delantero/src/roboclaw.h
@@ -0,0 +1,329 @@
+#pragma once
+
+#include <chrono>
+#include <rclcpp/logger.hpp>
+#include <string>
+
+#include "ros2_roboclaw_driver_delantero/srv/reset_encoders.hpp"
+
+/* There are possibly several clients that want to read sensor data,
+   such as encoder values, motor speeds, etc. Each time an actual read
+   from the RoboClaw device is attempted, it potentially conflicts with
+   motor commands. And it's possible that clients want sensor data more
+   often than reasonably expected.
+
+   As a result, GetXxx methods below that provide sensor data values are
+   broken up into a GetXxx and cache_GetXxx methods. The GetXxx methods
+   just return that last reading fetched from a periodic loop. The
+   cache_GetXxx methods do the real command request to the RoboClaw and
+   are only called during the class constructor and a periodic, background
+   thread.
+*/
+
+class RoboClaw {
+ public:
+  // Bit positions used to build alarms.
+  enum {
+    kM1_OVER_CURRENT = 0x01,        // Motor 1 current sense is too high.
+    kM2_OVER_CURRENT = 0x02,        // Motor 2 current sense is too high.
+    kM1_OVER_CURRENT_ALARM = 0x04,  // Motor 1 controller over current alarm.
+    kM2_OVER_CURRENT_ALARM = 0x08,  // Motor 2 controller over current alarm.
+  };
+
+  // Referencing which encoder in the RoboClaw
+  typedef enum WHICH_ENC {
+    kGETM1ENC = 16,
+    kGETM2ENC = 17,
+
+  } WHICH_ENC;
+
+  // Referencing which motor in the RoboClaw
+  typedef enum WHICH_MOTOR {
+    kGETM1PID = 55,
+    kGETM2PID = 56,
+  } WHICH_MOTOR;
+
+  // Referencing which velocity in the RoboClaw
+  typedef enum WHICH_VELOCITY {
+    kGETM1SPEED = 18,
+    kGETM2SPEED = 19,
+  } WHICH_VELOCITY;
+
+  // A convenience struction to pass around configuration information.
+  typedef struct {
+    float p;
+    float i;
+    float d;
+    uint32_t qpps;
+    float max_current;
+  } TPIDQ;
+
+  // For a custom exception message.
+  struct TRoboClawException : public std::exception {
+    std::string s;
+    TRoboClawException(std::string ss) : s(ss) {}
+    ~TRoboClawException() throw() {}
+    const char *what() const throw() { return s.c_str(); }
+  };
+
+  // Holds RoboClaw encoder result.
+  typedef struct {
+    int32_t value;
+    uint8_t status;
+  } EncodeResult;
+
+  // Constructor.
+  RoboClaw(const TPIDQ m1Pid, const TPIDQ m2Pid, float m1MaxCurrent,
+           float m2MaxCurrent, std::string device_name, uint8_t device_port,
+           uint8_t vmin, uint8_t vtime);
+
+  ~RoboClaw();
+
+  void doMixedSpeedDist(int32_t m1_quad_pulses_per_second,
+                        int32_t m1_max_distance,
+                        int32_t m2_quad_pulses_per_second,
+                        int32_t m2_max_distance);
+
+  void doMixedSpeedAccelDist(uint32_t accel_quad_pulses_per_second,
+                             int32_t m1_quad_pulses_per_second,
+                             uint32_t m1_max_distance,
+                             int32_t m2_quad_pulses_per_second,
+                             uint32_t m2_max_distance);
+
+  EncodeResult getEncoderCommandResult(WHICH_ENC command);
+
+  // Get RoboClaw error status bits.
+  uint16_t getErrorStatus();
+
+  // Get RoboClaw error status as a string.
+  std::string getErrorString();
+
+  float getLogicBatteryLevel();
+
+  float getMainBatteryLevel();
+
+  // Get the encoder value for motor 1.
+  int32_t getM1Encoder();
+
+  // Get the encoder value for motor 2.
+  int32_t getM2Encoder();
+
+  // Convenience structure to  hold a pair of current values.
+  typedef struct {
+    float m1Current;
+    float m2Current;
+  } TMotorCurrents;
+
+  // Make sure you call getMotorCurrents before getMotorAlarms.
+  int getMotorAlarms() { return motorAlarms_; }
+
+  TMotorCurrents getMotorCurrents();
+
+  TPIDQ getPIDQ(WHICH_MOTOR whichMotor);
+
+  float getTemperature();
+
+  // Get velocity (speed) of a motor.
+  int32_t getVelocity(WHICH_VELOCITY whichVelocity);
+
+  // Get RoboClaw software versions.
+	std::string getVersion();
+  
+  // Stop motion.
+  void stop();
+
+  // Get singleton instance of class.
+  static RoboClaw *singleton();
+
+  static void readSensorGroup();
+  
+ private:
+  EncodeResult cache_getEncoderCommandResult(WHICH_ENC command);
+
+  // Get RoboClaw error status bits.
+  uint16_t cache_getErrorStatus();
+
+  // Get RoboClaw error status as a string.
+  std::string cache_getErrorString();
+
+  float cache_getLogicBatteryLevel();
+
+  // Get the encoder value for motor 1.
+  int32_t cache_getM1Encoder();
+
+  int32_t cache_getM1Speed();
+
+  // Get the encoder value for motor 2.
+  int32_t cache_getM2Encoder();
+
+  int32_t cache_getM2Speed();
+
+  float cache_getMainBatteryLevel();
+
+  TMotorCurrents cache_getMotorCurrents();
+
+  TPIDQ cache_getPIDQ(WHICH_MOTOR whichMotor);
+
+  float cache_getTemperature();
+
+  // Get velocity (speed) of a motor.
+  int32_t cache_getVelocity(WHICH_VELOCITY whichVelocity);
+
+  static void sensorReadThread();
+
+  typedef struct {
+    uint16_t error_status;
+    std::string error_string;
+    float logic_battery_level;
+    int32_t m1_encoder;
+    EncodeResult m1_encoder_command_result;
+    TPIDQ m1_pidq;
+    int32_t m1_velocity;
+    int32_t m2_encoder;
+    EncodeResult m2_encoder_command_result;
+    TPIDQ m2_pidq;
+    int32_t m2_speed;
+    float main_battery_level;
+    int motor_alarms;
+    TMotorCurrents motor_currents;
+    float temperature;
+    std::chrono::system_clock::time_point last_sensor_read_time_;
+  } SensorValueGroup;
+
+  static SensorValueGroup g_sensor_value_group_;
+
+  typedef struct {
+    unsigned long p1;
+    unsigned long p2;
+  } ULongPair;
+
+  enum {
+    kERROR_NORMAL = 0x00,
+    kM1OVERCURRENT = 0x01,
+    kM2OVERCURRENT = 0x02,
+    kESTOP = 0x04,
+    kTEMPERATURE = 0x08,
+    kMAINBATTERYHIGH = 0x10,
+    kMAINBATTERYLOW = 0x20,
+    kLOGICBATTERYHIGH = 0x40,
+    kLOGICBATTERYLOW = 0x80
+  };
+
+  // Enum values without a 'k' prefix have not been used in code.
+  typedef enum ROBOCLAW_COMMAND {
+    M1FORWARD = 0,
+    M1BACKWARD = 1,
+    SETMINMB = 2,
+    SETMAXMB = 3,
+    M2FORWARD = 4,
+    M2BACKWARD = 5,
+    M17BIT = 6,
+    M27BIT = 7,
+    MIXEDFORWARD = 8,
+    MIXEDBACKWARD = 9,
+    MIXEDRIGHT = 10,
+    MIXEDLEFT = 11,
+    MIXEDFB = 12,
+    MIXEDLR = 13,
+    RESETENC = 20,
+    kGETVERSION = 21,
+    kSETM1ENCODER = 22,
+    kSETM2ENCODER = 23,
+    kGETMBATT = 24,
+    kGETLBATT = 25,
+    SETMINLB = 26,
+    SETMAXLB = 27,
+    kSETM1PID = 28,
+    kSETM2PID = 29,
+    GETM1ISPEED = 30,
+    GETM2ISPEED = 31,
+    M1DUTY = 32,
+    M2DUTY = 33,
+    kMIXEDDUTY = 34,
+    M1SPEED = 35,
+    M2SPEED = 36,
+    kMIXEDSPEED = 37,
+    M1SPEEDACCEL = 38,
+    M2SPEEDACCEL = 39,
+    MIXEDSPEEDACCEL = 40,
+    M1SPEEDDIST = 41,
+    M2SPEEDDIST = 42,
+    kMIXEDSPEEDDIST = 43,
+    M1SPEEDACCELDIST = 44,
+    M2SPEEDACCELDIST = 45,
+    kMIXEDSPEEDACCELDIST = 46,
+    GETBUFFERS = 47,
+    SETPWM = 48,
+    kGETCURRENTS = 49,
+    MIXEDSPEED2ACCEL = 50,
+    MIXEDSPEED2ACCELDIST = 51,
+    M1DUTYACCEL = 52,
+    M2DUTYACCEL = 53,
+    MIXEDDUTYACCEL = 54,
+    kGETTEMPERATURE = 82,
+    kGETERROR = 90,
+    WRITENVM = 94,
+    GETM1MAXCURRENT = 135
+  } ROBOCLAW_COMMAND;
+
+  int device_port_;  // Unix file descriptor for RoboClaw connection.
+  float m1p_;
+  float m1i_;
+  float m1d_;
+  int m1qpps_;
+  float m2p_;
+  float m2i_;
+  float m2d_;
+  int m2qpps_;
+  int maxCommandRetries_;    // Maximum number of times to retry a RoboClaw
+                             // command.
+  float maxM1Current_;       // Maximum allowed M1 current.
+  float maxM2Current_;       // Maximum allowed M2 current.
+  int motorAlarms_;          // Motors alarms. Bit-wise OR of contributors.
+  std::string device_name_;  // Device name of RoboClaw device.
+  int portAddress_;          // Port number of RoboClaw device under control
+  int vmin_;                 // Terminal control value.
+  int vtime_;                // Terminal control value.
+
+  // Get velocity (speed) result from the RoboClaw controller.
+  int32_t getVelocityResult(uint8_t command);
+
+  unsigned long getUlongCommandResult(uint8_t command);
+
+  uint32_t getULongCont(uint16_t &crc);
+
+  unsigned short get2ByteCommandResult(uint8_t command);
+
+  // Open the RoboClaw USB port.
+  void openPort();
+
+  // Read one byte from device with timeout.
+  uint8_t readByteWithTimeout();
+
+  // Perform error recovery to re-open a failed device port.
+  void restartPort();
+
+  // Reset the encoders.
+  bool resetEncoders(
+      ros2_roboclaw_driver_delantero::srv::ResetEncoders::Request &request,
+      ros2_roboclaw_driver_delantero::srv::ResetEncoders::Response &response);
+
+  // Set the PID for motor M1.
+  void setM1PID(float p, float i, float d, uint32_t qpps);
+
+  // Set the PID for motor M1.
+  void setM2PID(float p, float i, float d, uint32_t qpps);
+
+  // Update the running CRC result.
+  void updateCrc(uint16_t &crc, uint8_t data);
+
+  // Write one byte to the device.
+  void writeByte(uint8_t byte);
+
+  // Write a stream of bytes to the device.
+  void writeN(bool sendCRC, uint8_t cnt, ...);
+
+  void SetEncoder(ROBOCLAW_COMMAND command, long value);
+
+  static RoboClaw *g_singleton;
+};
